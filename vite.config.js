6552915bd59758c0a2f242d050792cfa1dc88ecb
Robin van Baalen <robin@ganttify.com>
2025-03-20 20:02:43 -0400

    fix: add Vite configuration and Tailwind CSS integration

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "node:path"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  base: "/eslint-config/", // Change this if your page is not hosted at a root URL
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+})
